c5cd8e2da18ed725c9eece7ae19a6dbff7a9a375
Shishir Mishra <shishir28@gmail.com>
2022-03-03 23:07:21 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      React Shopify Store
+    </div>
+  );
+}
+
+export default App;
---
+++ src/context/shopContext.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import Client from "shopify-buy";
+
+const ShopContext = React.createContext();
+
+const client = Client.buildClient({
+    domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
+    storefrontAccessToken: process.env.REACT_APP_SHOPIFY_STOREFRONT_ACCESS_TOKEN
+});
+console.log(client);
+
+class ShopProvider extends Component {
+    state = {
+        product: {},
+        products: [],
+        checkout: {},
+        isCartOpen: false,
+        isMenuOpen: false,
+    }
+
+    componentDidMount() {
+        if (localStorage.checkout_id) {
+            this.fetchCheckout(localStorage.checkout_id);
+        } else {
+            this.createCheckout()
+        }
+    }
+
+    createCheckout = async () => {
+        client.checkout.create().then((checkout) => {
+            localStorage.setItem('checkout_id', checkout.id);
+            this.setState({
+                checkout: checkout
+            })
+        })
+    }
+
+    fetchCheckout = async (checkoutId) => {
+        client.checkout
+            .fetch(checkoutId)
+            .then((checkout) => {
+                this.setState({ checkout: checkout });
+            })
+            .catch((error) => console.log(error));
+    };
+
+    addItemToCheckout = async (item) => {
+
+    }
+
+    removeLineItem = async (lineItemIdsToRemove) => {
+    }
+
+    fetchAllProducts = async () => {
+        const products = await client.product.fetchAll();
+        this.setState({
+            products: products
+        });
+    }
+
+    fetchProductWithHandle = async (handle) => {
+        const product = await client.product.fetchByHandle(handle);
+        this.setState({
+            product: product
+        });
+    }
+
+    closeCart = () => {
+
+    }
+
+    openCart = () => {
+
+    }
+
+    closeMenu = () => {
+    }
+
+    render() {
+        console.log(this.state);
+        return (
+            <ShopContext.Provider>
+                {this.props.children}
+            </ShopContext.Provider>
+        )
+    }
+}
+
+const ShopConsumer = ShopContext.Consumer;
+
+export { ShopConsumer, ShopContext };
+
+export default ShopProvider;
