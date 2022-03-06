1492df4ef742f93b622fb75ff83d5daf4f5ad70a
Shishir Mishra <shishir28@gmail.com>
2022-03-06 22:05:50 +1100

    CharaUI started using it

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { ShopContext } from '../context/shopContext'
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
 
 const Home = () => {
     const { fetchAllProducts, products } = useContext(ShopContext)
@@ -13,14 +14,26 @@ const Home = () => {
     }
 
     return (
-        <div>
-            {
-                products.map(product => (
-                    <Link to={`/products/${product.handle}`} key={product.id} >{product.title} </Link>
-                ))
-            }
-        </div>
+
+        <Box>
+            <Grid templateColumns="repeat(3,1fr)">
+                {
+                    products.map(product => (
+                        <Link to={`/products/${product.handle}`} key={product.id} >{product.title}
+                            <Box _hover={{ opacity: '80%' }} textAlign="center">
+
+                                <Image src={product.images[0].src} />
+                                <Text>{product.title}</Text>
+                                <Text>{product.variants[0].price}</Text>
+
+                            </Box>
+
+
+                        </Link>
+                    ))
+                }
+            </Grid>
+        </Box>
     )
 }
-
 export default Home
